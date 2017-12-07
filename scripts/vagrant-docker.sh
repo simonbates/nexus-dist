4eaf35ab07a516ae99bbcf221dbc3c70fe314f6c
Giovanni Tirloni <giovanni.tirloni@gmail.com>
2017-12-07 15:22:29 -0200

    Enforce seccomp policy for container

--- scripts/vagrant-docker.sh
+++ scripts/vagrant-docker.sh
@@ -2,19 +2,22 @@
 
 set -e
 
+VAGRANT_HOME="/vagrant"
+
 NEXUS_PORT="9081"
 NEXUS_IMAGE="gpii/nexus"
 NEXUS_CONTAINER="gpii-nexus"
-
-# Build Docker image
-docker build -t $NEXUS_IMAGE . 
+NEXUS_SECCOMP="nexus-seccomp.json"
 
 # Stop and remove previous Docker image and container
 docker rmi -f $NEXUS_IMAGE 2>/dev/null || true
 docker rm -f $NEXUS_CONTAINER 2>/dev/null || true
 
+# Build Docker image
+docker build -t $NEXUS_IMAGE .
+
 # Start Docker container
-docker run -d --name $NEXUS_CONTAINER -p $NEXUS_PORT:$NEXUS_PORT $NEXUS_IMAGE
+docker run -d --name $NEXUS_CONTAINER --security-opt seccomp=${VAGRANT_HOME}/${NEXUS_SECCOMP} -p $NEXUS_PORT:$NEXUS_PORT $NEXUS_IMAGE
 
 echo
 echo 
